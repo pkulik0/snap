24415e8ed4d64dde4fe0d19f8713903c83a2d289
pkulik0 <pkulik0@icloud.com>
2022-09-10 19:55:28 +0200

    Initial commit

---
+++ src/test/java/dev/pkulik/snap/SnapApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pkulik.snap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
