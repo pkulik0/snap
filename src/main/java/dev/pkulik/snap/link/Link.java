cad4b87262f441dbc145dc66f28795873623f0c8
pkulik0 <pkulik0@icloud.com>
2022-09-13 18:34:00 +0200

    Link: fully switch to annotations

--- src/main/java/dev/pkulik/snap/link/Link.java
+++ src/main/java/dev/pkulik/snap/link/Link.java
@@ -2,10 +2,15 @@ package dev.pkulik.snap.link;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotNull;
+
 @Data
+@Getter @Setter
 @AllArgsConstructor
 @Document(collection = "links")
 public class Link {
@@ -13,9 +18,6 @@ public class Link {
     @Id
     private String shortened;
 
+    @NotNull
     private String url;
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 }
